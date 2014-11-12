969dd306dfa91dfb64d45cca5ce6be98a79e7df9
James Williams <jamesrobertwilliams@gmail.com>
2014-11-12 21:42:46 +0000

    Changed the build mechanism slightly - now outputs into build folder. Ids slightly different.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,35 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+            dist: {
+                src: [
+                    "src/begin.js",
+                    "src/modules/annotation.js",
+                    "src/modules/table.js",
+                    "src/modules/chart.js",
+                    "src/end.js"
+                ],
+                dest: 'build/<%= pkg.name %>.<%= pkg.version %>.js'
+            }
+        },
+		uglify: {
+		  	options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		  	},
+		  	build: {
+				src: 'build/<%= pkg.name %>.<%= pkg.version %>.js',
+				dest: 'build/<%= pkg.name %>.<%= pkg.version %>.min.js'
+		  	}
+		}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load plugins for tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  	// Default task(s).
+  	grunt.registerTask('default', ['concat','uglify']);
 };
